ebf4c13fd21f65ddbec0f10251a0dcaef2178a84
huyeduon <huyeduon@cisco.com>
2021-02-02 13:20:01 +0800

    Edit client id

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "azurerm" {
     subscription_id = var.azure_demo04_subs
-    client_id = var.client_idhuyeduon_api_client_id
+    client_id = var.huyeduon_api_client_id
     client_secret = var.huyeduon_api_client_secret
     tenant_id = var.azure_tenant_id
     features {}
